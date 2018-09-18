2535987738f3bbded58bbc4acbf592dc6fc176db
Budnikov Sergey <gig019@yandex.ru>
2018-09-18 14:08:28 +0400

    Add files via upload

--- application/plugins/admin-page/views/admin-page/units/data-rows.php
+++ application/plugins/admin-page/views/admin-page/units/data-rows.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 //pr($all_data_types);
 ?>
-<div class="col-inner-2 show-hide-switch">
+<div class="col-inner-2 show-hide-switch" data-sh-id="ap_6">
 <h2>Типовые поля</h2>
 <?
 //pr($all_data_types);
@@ -13,24 +13,24 @@ if(isset($all_data_types[1])):
 		{
 			$value = $data_fields[$fval['types_fields_id']]['objects_data_value'];
 		}
-	
+
 	?>
 	<?
 		$styleFGroup = '';
 		if($fval['types_fields_type'] == 'editor')
 			$styleFGroup = ' style="flex-basis:100%"';
-	
-	
+
+
 	?>
 		 <div class="form-group"<?= $styleFGroup ?>>
 			<label for="data_field_i_<?=$i?>"><?=$fval['types_fields_name']?></label>
 			<? if($fval['types_fields_type'] == 'text'): ?>
 			<input type="text" class="form-control" id="data_field_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'textarea'): ?>
 			<textarea class="form-control" id="data_field_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]"><?=$value?></textarea>
-		   
-			
+
+
 			<? elseif($fval['types_fields_type'] == 'select'): ?>
 				<?
 				$options = app_parse_select_values($fval['types_fields_values']);
@@ -40,32 +40,32 @@ if(isset($all_data_types[1])):
 						'id' => 'data_field_' . $i,
 						'name' => 'data_field[' . $obj_id . '][' . $fval['types_fields_id'] . ']',
 					);
-		   
+
 				echo form_dropdown($args, $options, $value)
 			 ?>
-			
+
 			<? // изображение ?>
          <? elseif($fval['types_fields_type'] == 'image'): ?>
-         
+
          	<input type="text" class="form-control field-image" id="data_field_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
-       
-			
+
+
 			<? elseif($fval['types_fields_type'] == 'file'): ?>
 			<input type="text" class="form-control field-image elf-file" id="data_field_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'date'): ?>
 			<input type="text" class="form-control field-date" id="data_field_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>" data-date-format="<?= app_get_option('date_format', 'general', 'YYYY-MM-DD'); ?>"/>
-			
-			
+
+
 			<? elseif($fval['types_fields_type'] == 'datetime'): ?>
 			<input type="text" class="form-control field-date fd-time" id="data_field_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>" data-date-format="<?= app_get_option('date_format', 'general', 'YYYY-MM-DD'); ?> HH:mm:ss"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'number'): ?>
 			<input type="text" class="form-control field-number" id="data_field_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'numberfloat'): ?>
 			<input type="text" class="form-control field-numberfloat" id="data_field_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'multiselect'): ?>
 			<?
 			 	$value = explode(',', $value);
@@ -75,15 +75,15 @@ if(isset($all_data_types[1])):
 						'id' => 'data_field_' . $i,
 						'name' => 'data_field[' . $obj_id . '][' . $fval['types_fields_id'] . '][]',
 					);
-		   
+
 				echo form_multiselect($args, $options, $value);
 			 ?>
-			 
+
 			 <? elseif($fval['types_fields_type'] == 'editor'): ?>
 			<textarea class="form-control editor data-rows-editor" id="data_field_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]"><?=$value?></textarea>
-			 
-			 
-			
+
+
+
 			<? endif; ?>
 		</div>
 	<?
@@ -94,7 +94,7 @@ endif;
 </div>
 
 <? if(isset($all_data_types[$obj_data_type]) and $obj_data_type !== '1'): ?>
-<div class="col-inner-2 show-hide-switch">
+<div class="col-inner-2 show-hide-switch" data-sh-id="ap_7">
 <h2>Поля типа данных</h2>
 <?
 	$i = 1;
@@ -111,7 +111,7 @@ endif;
 		$styleFGroup = '';
 		if($fval['types_fields_type'] == 'editor')
 			$styleFGroup = ' style="flex-basis:100%"';
-		
+
 	?>
 		 <div class="form-group"<?= $styleFGroup ?>>
 			<label for="data_field_i_<?=$i?>"><?= $fval['types_fields_name'] ?>
@@ -119,14 +119,14 @@ endif;
 					(<?= $fval['types_fields_unit'] ?>)
 				<? endif ?>
 			</label>
-			
+
 			<? if($fval['types_fields_type'] == 'text'): ?>
 			<input type="text" class="form-control" id="data_field_i_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'textarea'): ?>
 			<textarea class="form-control" id="data_field_i_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]"><?=$value?></textarea>
-		   
-			
+
+
 			<? elseif($fval['types_fields_type'] == 'select'): ?>
 				<?
 				$options = app_parse_select_values($fval['types_fields_values']);
@@ -136,31 +136,31 @@ endif;
 						'id' => 'data_field_i_' . $i,
 						'name' => 'data_field[' . $obj_id . '][' . $fval['types_fields_id'] . ']',
 					);
-		   
+
 				echo form_dropdown($args, $options, $value)
 			 ?>
-			 
+
 			<? elseif($fval['types_fields_type'] == 'image'): ?>
-         
+
          <input type="text" class="form-control field-image" id="data_field_i_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
         <!-- <button class="btn btn-danger image-dialog" type="button" data-input-path="data_field_i_<?=$i?>">Загрузить</button>  -->
-			
+
 			<? elseif($fval['types_fields_type'] == 'file'): ?>
 			<input type="text" class="form-control field-image elf-file" id="data_field_i_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'date'): ?>
 			<input type="text" class="form-control field-date" id="data_field_i_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>" data-date-format="<?= app_get_option('date_format', 'general', 'YYYY-MM-DD'); ?>"/>
-			
-			
+
+
 			<? elseif($fval['types_fields_type'] == 'datetime'): ?>
 			<input type="text" class="form-control field-date fd-time" id="data_field_i_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>" data-date-format="<?= app_get_option('date_format', 'general', 'YYYY-MM-DD'); ?> HH:mm:ss"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'number'): ?>
 			<input type="text" class="form-control field-number" id="data_field_i_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'numberfloat'): ?>
 			<input type="text" class="form-control field-numberfloat" id="data_field_i_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]" value="<?=$value?>"/>
-			
+
 			<? elseif($fval['types_fields_type'] == 'multiselect'): ?>
 			<?
 				$value = explode(',', $value);
@@ -170,14 +170,14 @@ endif;
 						'id' => 'data_field_' . $i,
 						'name' => 'data_field[' . $obj_id . '][' . $fval['types_fields_id'] . '][]',
 					);
-		   
+
 				echo form_multiselect($args, $options, $value)
 			 ?>
 			<? elseif($fval['types_fields_type'] == 'editor'): ?>
 			<textarea class="form-control editor data-rows-editor" id="data_field_i_<?=$i?>" name="data_field[<?=$obj_id?>][<?=$fval['types_fields_id']?>]"><?=$value?></textarea>
-			
+
 			<? endif; ?>
-			 </div> 
+			 </div>
 	<?
 	$i++;
 	endforeach;
